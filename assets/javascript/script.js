36bb3c06f0691ed3cb73bd9518434c6e67069c24
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2015-07-24 17:41:29 +0530

    Anagram solver for character names now enabled

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,23 @@
-var chars;
+//Loading JSON Files
+var chars,sorted;
 $.getJSON( "https://raw.githubusercontent.com/AniMysore74/got-quiz-guide/gh-pages/assets/json/chars.json", function(data) {
   	chars = data;
 })
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function() {
-    console.log( "error" );
-  })
-  .always(function() {
-    console.log( "complete" );
-  });
+.done(function() {
+    console.log( "C.H.A.R.S Loaded" );
+})
+.fail(function() {
+    console.log( "Error loading Json" );
+})
+$.getJSON( "https://raw.githubusercontent.com/AniMysore74/got-quiz-guide/gh-pages/assets/json/sorted.json", function(data) {
+  sorted = data;
+})
+.done(function() {
+    console.log( "S.O.R.T.E.D Loaded" );
+})
+.fail(function() {
+    console.log( "Error loading Json" );
+})
  	//FORM HANDLER 
  	function formHandler(form,defaultVal){
  		//Upon receving form
@@ -21,64 +28,49 @@ $.getJSON( "https://raw.githubusercontent.com/AniMysore74/got-quiz-guide/gh-page
 				return;
 			//Checks if Form is ready for processing
 		(function blankchecker(){
-			if(!form.rad[0].checked){
-				display("Please choose how you want to search");
+			if(!form.rad[0].checked && !form.rad[2].checked){
+				display({text:"Please choose how you want to search"});
 				return;
 			}
 			if(!form.names.checked) {
-				display("Please choose what to search from");
+				display({text:"Please choose what to search from"});
 				return;
 			}
 			if(form.searchField==null){
-				display("Please enter what to search");
+				display({text:"Please enter what to search"});
 				return;
 			}
 		})();
  		if(form.rad[0].checked)
- 			initialsSearch(form);
+ 			initialsSearch(form.searchField.value);
  		else if(form.rad[1].checked)
  			hangmanSearch();
  		else if(form.rad[2].checked)
- 			anagramSearch();
+ 			anagramSearch(form.searchField.value);
  	}
 
-	function initialsSearch(form)
+	function initialsSearch(initials)
 	{
-		//INITIALS
-		var count = 0;
-		console.log("Searching by initials")
+		var searchcounter = 0;
 		if(form.names.checked)
-			console.log("through Names")
 			for(var x in chars){
 				var check= chars[x].name.split(" ");
 				var flag=true;   
-				var initials=form.searchField.value;
-
-				console.log(chars[x].name+" "+initials);
-
 				if(!(initials.length!==check.length)){		
 					for(var y in check){
-						console.log(check[y]+" "+check[y].charAt(0)+" vs "+initials.charAt(y));
 						if(check[y].charAt(0)!==initials.charAt(y).toLocaleUpperCase()){
 								flag=false;
-								console.log(flag+"Checker loop");
 						}
 			        }
 			    }
 			    else
 			    {
-			    	flag=false;
-			    	console.log("Lengths not matching")
-			    	console.log(flag+"Position loop");
-			    
+			    	flag=false;		    
 			    }
 				if(flag){
-					console.log(chars[x].name);
-					display(null,x,{name:true,description:true});
-					count++;
+					display({dataObject:true,name:true,description:true},chars[x]);
+					searchcounter++;
 				}
-				else	
-					console.log("No name Found");
 				/*if(form.alias.checked && chars[x].alias[0]!=null)
 					console.log("and through Aliases")
 					
@@ -88,59 +80,55 @@ $.getJSON( "https://raw.githubusercontent.com/AniMysore74/got-quiz-guide/gh-page
 						count++;
 					}*/
 			}
-		if(count==0){
+		if(searchcounter==0){
 			noResult();
 		}
+		function titleSearch(initials, x){
+			var flag = 1;
+			for(var y in chars[x].alias){
+				var check= chars[x].alias[y].split(" ");   
+				for(var z in check){
+					if(check[z].charAt(0)!==initials.charAt(z)){
+							flag=0;
+					}
+		        }
+		    }
+			if(flag)
+				return chars[x].alias[z];
+			else
+				return 0;
+		}
 	}
-	function titleSearch(initials, x){
-		var flag = 1;
-		for(var y in chars[x].alias){
-			var check= chars[x].alias[y].split(" ");   
-			for(var z in check){
-				if(check[z].charAt(0)!==initials.charAt(z)){
-						flag=0;
-				}
-	        }
-	    }
-		if(flag)
-			return chars[x].alias[z];
-		else
-			return 0;
+
+	function anagramSearch(inputAnagram)
+	{
+		var searchcounter=0;
+		var anagramSorted= inputAnagram.toLocaleUpperCase().split('').sort().join('').trim();
+		console.log(anagramSorted);
+		for(var x in sorted)
+		{
+			if(sorted[x].sort===anagramSorted)
+			{
+				display({dataObject:true,name:true},sorted[x])
+			}
+		}
 	}
 	function noResult(){
 		$('.results').removeClass('show');
 		$('.results').addClass('show');
 		$('<div class = "panel"> Nothing Found! Valar Morghulis </div>').appendTo('.results .columns');
 	}
-	function display(text, pos, params){
+	function display(params, dataObject){
 		$('.results').removeClass('show');
 		$('.results').addClass('show');
-		if(text!==null)
-			$('<div class = "panel">'+text+'</div>').appendTo('.results .columns');
-		else if(params.name && params.description && chars[pos].description!="")
-			$('<div class = "panel"><span class = "name">'+chars[pos].name+'</span>'+" "+chars[pos].description+'</div>').appendTo('.results .columns');
-		else if(params.name)
-			$('<div class = "panel"><span class = "name">'+chars[pos].name+'</span></div>').appendTo('.results .columns');
+		if(params.hasOwnProperty("text"))
+			$('<div class = "panel">'+params.text+'</div>').appendTo('.results .columns');
+		else if(params.hasOwnProperty("dataObject"))
+		{
+			if(params.name && params.description && dataObject.hasOwnProperty("description"))
+				$('<div class = "panel"><span class = "name">'+dataObject.name+'</span>'+" "+dataObject.description+'</div>').appendTo('.results .columns');
+			else if(params.name)
+				$('<div class = "panel"><span class = "name">'+dataObject.name+'</span></div>').appendTo('.results .columns');
+		}
+		
 	}
-
-	/*function getNamebyAnagram()
-	{
-		//Anagrams
-		var  permutations= permutation(anagram);
-		//permutation = temp.split(" ");
-		console.log(permutations);
-		for(var x in chars){
-			check=chars[x].name.split(",").join("").toUppercase();
-			var flag=1;
-			for(var y in permutations){
-				if(check===permutations[y].toUppercase())
-					flag=0;
-			}
-			if(flag)
-				console.log(chars[x].name+" "+chars[x].alias);
-			else	
-				console.log("Not Found");
-		}*/
-	//getNamebyAnagram();
-
-	
